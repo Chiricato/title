16e3dd07435c31975f9522d43cfdc5b70bf546b6
Chiricato <levantan.vn.it@gmail.com>
2024-10-03 14:59:16 +0700

    Create title.py

---
+++ title.py
@@ -0,0 +1,12 @@
+BLUE = "\33[94m"
+RESET = "\033[0m"
+banner = f"""{BLUE}                                                       
+TTTTTTTT  AAAAA   N   N      LL      EEEEEE  
+   TT     A   A   NN  N      LL      EE      
+   TT     AAAAA   N N N      LL      EEEEEE   
+   TT     A   A   N  NN      LL      EE      
+   TT     A   A   N   N      LLLLL   EEEEEE
+
+{RESET}"""
+
+print(banner)
